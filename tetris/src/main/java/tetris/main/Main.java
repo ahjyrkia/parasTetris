b6494b91d928eda9d86b415a7c34e5adcc94167b
anton <anton.jyrkiainen@helsinki.fi>
2015-01-19 14:46:34 +0200

    testi

--- tetris/src/main/java/tetris/main/Main.java
+++ tetris/src/main/java/tetris/main/Main.java
@@ -12,9 +12,7 @@ package tetris.main;
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         System.out.println("kakka");
     }
